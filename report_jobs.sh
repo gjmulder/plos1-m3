5ccacaa5ecf91df450074c232e91b8b653465bb9
Gary Mulder <gjmulder@gmail.com>
2019-11-16 16:48:03 +0200

    Backslashed param

--- report_jobs.sh
+++ report_jobs.sh
@@ -10,5 +10,5 @@ fi
 echo "sMAPE,experiment,start.date,end.date"
 for DB in plos1-m3-001g plos1-m3-002g
 do
-	echo "db.jobs.find({"result.status" : "$STATUS"}).toArray()" | mongo --host heika $DB | awk '/loss/ {printf("%.4f,", $NF)} /exp_key/ {printf("%s", $NF)} /book_time/ {printf("%10s,", substr($NF, 10, 24))} /refresh_time/ {print substr($NF, 10, 24)}'
+	echo "db.jobs.find({\"result.status\" : \"$STATUS\"}).toArray()" | mongo --host heika $DB | awk '/loss/ {printf("%.4f,", $NF)} /exp_key/ {printf("%s", $NF)} /book_time/ {printf("%10s,", substr($NF, 10, 24))} /refresh_time/ {print substr($NF, 10, 24)}'
 done | grep -v "^nan"
