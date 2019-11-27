fd7da0ebac5cdc47d207984a7b73e34c02f64ccb
Gary Mulder <gjmulder@gmail.com>
2019-11-27 14:11:34 +0200

    006g run for PLOS

--- mongo_results.R
+++ mongo_results.R
@@ -14,45 +14,45 @@ mongo_data %>%
 
 mongo_data %>%
   mutate(training.duration.secs = as.numeric(end.time - start.time)) %>%
-  arrange(desc(sMAPE)) %>%
+  arrange(desc(MASE)) %>%
   inner_join(model_counts) %>%
   mutate(rank = 1:nrow(mongo_data)) %>%
-  filter(sMAPE < 20) %>%
+  filter(MASE < 20) %>%
   mutate(experiment = as.factor(experiment)) ->
   mongo_plot_data
 
-dl_smape <- min(mongo_plot_data$sMAPE)
-ets_smape <- 7.12
-bnn_smape <- 7.96
+dl_MASE <- min(mongo_plot_data$MASE)
+ARIMA_MASE <- 0.89
+bnn_iter_MASE <- 0.93
 
 gg <- ggplot(mongo_plot_data) +
-  geom_hline(yintercept = dl_smape) +
-  geom_text(aes(x = 0, y = dl_smape - 0.3, hjust = "left"), size=3,
-            label = paste0("Deep Auto Regressive = ", round(dl_smape, 2))) +
-  geom_hline(yintercept = ets_smape) +
-  geom_text(aes(x = 0, y = ets_smape - 0.3, hjust = "left"), size=3,
-            label = paste0("ETS = ", ets_smape)) +
-  geom_hline(yintercept = bnn_smape) +
-  geom_text(aes(x = 0, y = bnn_smape - 0.3, hjust = "left"), size=3,
-            label = paste0("BNN = ", bnn_smape)) +
+  geom_hline(yintercept = dl_MASE) +
+  geom_text(aes(x = 0, y = dl_MASE - 0.015, hjust = "left"), size=3,
+            label = paste0("Deep Auto Regressive = ", round(dl_MASE, 2))) +
+  geom_hline(yintercept = ARIMA_MASE) +
+  geom_text(aes(x = 0, y = ARIMA_MASE - 0.015, hjust = "left"), size=3,
+            label = paste0("ARIMA = ", ARIMA_MASE)) +
+  geom_hline(yintercept = bnn_iter_MASE) +
+  geom_text(aes(x = 0, y = bnn_iter_MASE - 0.015, hjust = "left"), size=3,
+            label = paste0("BNN Iterative = ", bnn_iter_MASE)) +
   scale_y_log10()
 
 gg_rank <-
   gg +
-  geom_point(aes(x = rank, y = sMAPE, colour = experiment),
+  geom_point(aes(x = rank, y = MASE, colour = experiment),
              size = 2) +
   facet_grid(model.type.count ~ .) +
   xlab("Rank") +
-  ylab("sMAPE (log scale)") +
+  ylab("MASE (log scale)") +
   ggtitle("Ranked model accuracy")
 
 gg_duration <-
   gg +
-  geom_point(aes(x = training.duration.secs, y = sMAPE, colour = model.type.count),
+  geom_point(aes(x = training.duration.secs, y = MASE, colour = model.type.count),
              size = 2) +
-  geom_smooth(aes(x = training.duration.secs, y = sMAPE), se = FALSE) +
+  # geom_smooth(aes(x = training.duration.secs, y = MASE), se = FALSE) +
   xlab("Duration (secs)") +
-  ylab("sMAPE (log scale)") +
+  ylab("MASE (log scale)") +
   ggtitle("Trial duration versus model accuracy")
 
 print(gg_rank)
--- report_jobs.sh
+++ report_jobs.sh
@@ -7,9 +7,9 @@ else
 	STATUS=$1
 fi
 
-echo "sMAPE,model.type,experiment,start.time,end.time"
+echo "MASE,model.type,experiment,start.time,end.time"
 #for DB in plos1-m3-001g plos1-m3-002g plos1-m3-003g
-for DB in plos1-m3-004g
+for DB in plos1-m3-006g
 do
 	#echo "db.jobs.find({\"result.status\" : \"$STATUS\"}).toArray()" | mongo --host heika $DB | awk '/loss/ {printf("%8.4f,", $NF)} /exp_key/ {printf("%s", $NF)} /book_time/ {printf("%10s,", substr($NF, 10, 24))} /refresh_time/ {print substr($NF, 10, 24)}'
 	echo "db.jobs.find({\"result.status\" : \"$STATUS\"}).toArray()" | mongo --host heika $DB | awk '/loss/ {printf("%8.4f,", $NF)} /exp_key/ {printf("%s", $NF)} /"type"/ {printf("%10s,", $NF)} /book_time/ {printf("%10s", $NF)} /refresh_time/ {print $NF}'
