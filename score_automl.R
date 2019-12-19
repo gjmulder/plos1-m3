00c6c362e27cf52358b88b0f9532420fc2be2c35
Gary Mulder <gjmulder@gmail.com>
2019-12-19 10:41:12 +0200

    JSON results parsing working

--- score_automl.R
+++ score_automl.R
@@ -1,24 +1,47 @@
 library(Mcomp)
-library(lubridate)
+library(anytime)
 library(jsonlite)
 library(Metrics)
 library(tidyverse)
+library(purrr)
 
-results <- read_csv("3hours_15features.csv")
-# results
-df <-
-  results %>%
+results15 <- read_csv("3hours_15features.csv")
+
+df15 <-
+  results15 %>%
   filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = ymd(START_DATE)) %>%
+  mutate(start_date = anytime(START_DATE)) %>%
   select(start_date, IDX, Y, predicted_Y)
-
-mapes <-
-  results %>%
+mapes15 <-
+  results15 %>%
   filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = ymd(START_DATE)) %>%
+  mutate(start_date = anytime(START_DATE)) %>%
   select(start_date, IDX, Y, predicted_Y) %>%
   group_by(IDX) %>%
   top_n(-1, start_date) %>%
   mutate(ts_smape = smape(Y, predicted_Y))
 
-print(paste0("Mean sMAPE for test set: ", round(mean(mapes$ts_smape)*100, 2)))
+print(paste0("Mean sMAPE for 15 feature test set: ", round(mean(mapes15$ts_smape)*100, 2)))
+
+#############################################################################
+
+results_txt <- readLines("3hours_27features.json")
+results_list <- transpose(lapply(results_txt, fromJSON))
+results27 <- as.data.frame(lapply(results_list, unlist))
+
+df27 <-
+  results27 %>%
+  filter(DATA_SPLIT == "TEST") %>%
+  mutate(start_date = anytime(START_DATE)) %>%
+  select(start_date, IDX, Y, predicted_Y)
+
+mapes27 <-
+  results27 %>%
+  filter(DATA_SPLIT == "TEST") %>%
+  mutate(start_date = anytime(START_DATE)) %>%
+  select(start_date, IDX, Y, predicted_Y) %>%
+  group_by(IDX) %>%
+  top_n(-1, start_date) %>%
+  mutate(ts_smape = smape(Y, predicted_Y))
+
+print(paste0("Mean sMAPE for 27 feature test set: ", round(mean(mapes27$ts_smape)*100, 2)))
