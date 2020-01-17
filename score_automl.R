bb50b7ba67d2776e7db1562e4ac59aaed591ee75
Gary Mulder <gjmulder@gmail.com>
2020-01-17 08:29:20 +0200

    AutoMl scoring

--- score_automl.R
+++ score_automl.R
@@ -5,16 +5,121 @@ library(Metrics)
 library(tidyverse)
 library(purrr)
 
-results15 <- read_csv("15feature_automl_prediction.csv")
+# results15 <- read_csv("15feature_automl_prediction.csv")
+#
+# df15 <-
+#   results15 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y)
+#
+# mapes15 <-
+#   results15 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y) %>%
+#   group_by(IDX) %>%
+#   top_n(-1, start_date) %>%
+#   mutate(ts_smape = smape(Y, predicted_Y))
+#
+# print(paste0("Mean sMAPE for 3 hour 15 feature test set: ", round(mean(mapes15$ts_smape)*100, 2)))
+#
+# #############################################################################
+#
+# results27 <- read_csv("27feature_automl_prediction.csv")
+#
+# df27 <-
+#   results27 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y)
+#
+# mapes27 <-
+#   results27 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y) %>%
+#   group_by(IDX) %>%
+#   top_n(-1, start_date) %>%
+#   mutate(ts_smape = smape(Y, predicted_Y))
+#
+# print(paste0("Mean sMAPE for 3 hour 27 feature test set: ", mean(mapes27$ts_smape)*100, 2))
+#
+# #############################################################################
+#
+# results5_27 <- read_csv("5hour_27feature_automl_prediction.csv")
+#
+# df5_27<-
+#   results5_27 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y)
+#
+# mapes5_27 <-
+#   results5_27 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y) %>%
+#   group_by(IDX) %>%
+#   top_n(-1, start_date) %>%
+#   mutate(ts_smape = smape(Y, predicted_Y))
+#
+# print(paste0("Mean sMAPE for 3.7 hour 27 feature test set: ", mean(mapes5_27$ts_smape)*100, 2))
+#
+# #############################################################################
+#
+# results1_40 <- read_csv("1hour_40feature_automl_prediction.csv")
+#
+# df1_40 <-
+#   results1_40 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y)
+#
+# mapes1_40 <-
+#   results1_40 %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y) %>%
+#   group_by(IDX) %>%
+#   top_n(-1, start_date) %>%
+#   mutate(ts_smape = smape(Y, predicted_Y))
+#
+# print(paste0("Mean sMAPE for 1 hour 40 feature test set: ", mean(mapes1_40$ts_smape)*100, 2))
+#
+# #############################################################################
+#
+# results6_9 <- read_csv("6hour_9feature_automl_prediction.csv")
+#
+# df6_9 <-
+#   results6_9  %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y)
+#
+# mapes6_9  <-
+#   results6_9  %>%
+#   filter(DATA_SPLIT == "TEST") %>%
+#   mutate(start_date = anytime(START_DATE)) %>%
+#   select(start_date, IDX, Y, predicted_Y) %>%
+#   group_by(IDX) %>%
+#   top_n(-1, start_date) %>%
+#   mutate(ts_smape = smape(Y, predicted_Y))
+#
+# print(paste0("Mean sMAPE for 6 hour 9 feature test set: ", mean(mapes6_9$ts_smape)*100, 2))
+#
+# #############################################################################
 
-df15 <-
-  results15 %>%
+results9_40 <- read_csv("9hour_40_feature_automl_prediction.csv")
+
+df9_40 <-
+  results9_40  %>%
   filter(DATA_SPLIT == "TEST") %>%
   mutate(start_date = anytime(START_DATE)) %>%
   select(start_date, IDX, Y, predicted_Y)
 
-mapes15 <-
-  results15 %>%
+mapes9_40  <-
+  results9_40  %>%
   filter(DATA_SPLIT == "TEST") %>%
   mutate(start_date = anytime(START_DATE)) %>%
   select(start_date, IDX, Y, predicted_Y) %>%
@@ -22,91 +127,7 @@ mapes15 <-
   top_n(-1, start_date) %>%
   mutate(ts_smape = smape(Y, predicted_Y))
 
-print(paste0("Mean sMAPE for 3 hour 15 feature test set: ", round(mean(mapes15$ts_smape)*100, 2)))
-
-#############################################################################
-
-results27 <- read_csv("27feature_automl_prediction.csv")
-
-df27 <-
-  results27 %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y)
-
-mapes27 <-
-  results27 %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y) %>%
-  group_by(IDX) %>%
-  top_n(-1, start_date) %>%
-  mutate(ts_smape = smape(Y, predicted_Y))
-
-print(paste0("Mean sMAPE for 3 hour 27 feature test set: ", mean(mapes27$ts_smape)*100, 2))
-
-#############################################################################
-
-results5_27 <- read_csv("5hour_27feature_automl_prediction.csv")
-
-df5_27<-
-  results5_27 %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y)
-
-mapes5_27 <-
-  results5_27 %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y) %>%
-  group_by(IDX) %>%
-  top_n(-1, start_date) %>%
-  mutate(ts_smape = smape(Y, predicted_Y))
-
-print(paste0("Mean sMAPE for 3.7 hour 27 feature test set: ", mean(mapes5_27$ts_smape)*100, 2))
-
-#############################################################################
-
-results1_40 <- read_csv("1hour_40feature_automl_prediction.csv")
-
-df1_40 <-
-  results1_40 %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y)
-
-mapes1_40 <-
-  results1_40 %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y) %>%
-  group_by(IDX) %>%
-  top_n(-1, start_date) %>%
-  mutate(ts_smape = smape(Y, predicted_Y))
-
-print(paste0("Mean sMAPE for 1 hour 40 feature test set: ", mean(mapes1_40$ts_smape)*100, 2))
-
-#############################################################################
-
-results6_9 <- read_csv("6hour_9feature_automl_prediction.csv")
-
-df6_9 <-
-  results6_9  %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y)
-
-mapes6_9  <-
-  results6_9  %>%
-  filter(DATA_SPLIT == "TEST") %>%
-  mutate(start_date = anytime(START_DATE)) %>%
-  select(start_date, IDX, Y, predicted_Y) %>%
-  group_by(IDX) %>%
-  top_n(-1, start_date) %>%
-  mutate(ts_smape = smape(Y, predicted_Y))
-
-print(paste0("Mean sMAPE for 6 hour 9 feature test set: ", mean(mapes6_9$ts_smape)*100, 2))
+print(paste0("Mean sMAPE for 9 hour 40 feature test set: ", mean(mapes9_40$ts_smape)*100, 2))
 
 #############################################################################
 
